39b02c032caf155f5a3e8b573a1f94d6f627bd04
karthick <a.karthie+github@gmail.com>
2022-05-21 23:30:04 +0100

    :recycle: re-work logging

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,43 +1,23 @@
 package logging
 
 import (
+	"sync"
+
 	"go.uber.org/zap"
 )
 
-//go:generate mockgen -package=mocks -source=${GOFILE} -destination=mocks/${GOFILE}
-// Logger interface to create logger
-type Logger interface {
-	With(args ...interface{}) *zap.SugaredLogger
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	DPanic(args ...interface{})
-	Panic(args ...interface{})
-	Fatal(args ...interface{})
-	Debugf(template string, args ...interface{})
-	Infof(template string, args ...interface{})
-	Errorf(template string, args ...interface{})
-	DPanicf(template string, args ...interface{})
-	Panicf(template string, args ...interface{})
-	Fatalf(template string, args ...interface{})
-	Debugw(msg string, keysAndValues ...interface{})
-	Infow(msg string, keysAndValues ...interface{})
-	Warnw(msg string, keysAndValues ...interface{})
-	Errorw(msg string, keysAndValues ...interface{})
-	DPanicw(msg string, keysAndValues ...interface{})
-	Panicw(msg string, keysAndValues ...interface{})
-	Fatalw(msg string, keysAndValues ...interface{})
-}
+var (
+	once = &sync.Once{}
+	// GlobalLogger is the zap logger used across application
+	GlobalLogger *zap.Logger
+)
 
-// PkgLogger holds logger for service
-type PkgLogger struct {
-	Logger
-}
-
-// NewLogger initializes a new instance of logger
-func NewLogger(logger Logger) (*PkgLogger, error) {
-	return &PkgLogger{
-		logger,
-	}, nil
+// New generates a singleton instance of logger
+func New() *zap.Logger {
+	once.Do(func() {
+		if GlobalLogger == nil {
+			GlobalLogger = zap.New(nil)
+		}
+	})
+	return GlobalLogger
 }
